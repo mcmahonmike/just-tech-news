298d149694a5b3a352cae78c5c4285521407ee70
Hephaestus <mcmahonmike9@yahoo.com>
2022-05-25 21:01:35 -0500

    user varification built

--- models/User.js
+++ models/User.js
@@ -45,6 +45,13 @@ User.init(
         }
       }
     },
+    // create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+},
     {
       hooks: {
         // set up beforeCreate lifecycle "hook" functionality
